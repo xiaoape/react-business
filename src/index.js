a31f690eff3daf4688e19a24dbc8877c00575b60
bruceliu <bruceliu@nswap.com>
2022-04-21 20:02:08 +0800

    feat: 添加react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import '@/index.css';
-import App from '@/App';
+import Home from '@/pages/home/index';
+import About from '@/pages/about/index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
