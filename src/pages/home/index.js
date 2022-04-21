69e0328c8630912457b2c7101bf2ccb4cdc38ea2
bruceliu <bruceliu@nswap.com>
2022-04-21 20:21:04 +0800

    feat: useDebounce功能测试

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,13 @@
+import { useCallback } from "react";
 import { Link } from "react-router-dom";
-
+import useDebounce from "../../utils/useDebounce";
 function Home() {
+  const handleClickOne = useDebounce(function() {
+    console.log('点击一')
+  }, 1000, [])
+  const handleClickTwo = useCallback(() => {
+    console.log('点击二')
+  }, [])
   return (
     <div>
       <h1>Home</h1>
@@ -8,6 +15,8 @@ function Home() {
         <Link to="/">Home</Link> |{" "}
         <Link to="about">About</Link>
       </nav>
+      <button onClick={handleClickOne}>clickOne</button>
+      <button onClick={handleClickTwo}>clickTwo</button>
     </div>
   );
 }
