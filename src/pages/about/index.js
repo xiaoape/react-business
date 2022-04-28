eda11d23ab31d9b6cd993e85cef055c6332646db
bruceliu <bruceliu@nswap.com>
2022-04-28 11:08:02 +0800

    feat: BigNumber功能测试

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,14 +1,19 @@
 import { previewShow } from '@/components/preview';
-
+import BigNumber from 'bignumber.js'
 function About() {
   const handleClick = () => {
     console.log('handleClick')
     previewShow()
   }
+  const x = new BigNumber(0.1)
+  const y = x.plus(0.2)
+  console.log(y, 'yyyyy')
   return (
     <div>
       <h1>about</h1>
       <button onClick={handleClick}>showModal</button>
+      <div>{`直接使用0.2 + 0.1: ${0.2 + 0.1}`}</div>
+      <div>{`使用BigNumber进行0.2 + 0.1: ${y.toNumber()}`}</div>
     </div>
   );
 }
